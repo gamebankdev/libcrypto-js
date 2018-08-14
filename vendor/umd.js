a4ea5f352c6c76f447194311762557940ae6d885
Pecking <291375086@qq.com>
2018-08-14 17:58:16 +0800

    '1'

---
+++ vendor/umd.js
@@ -0,0 +1,13 @@
+(function(root, factory) {
+  if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    /* global require */
+    factory(exports);
+  } else {
+    root.gamebank = root.gamebank || {};
+    factory((root.gamebank.sjcl = {}));
+  }
+})(this, function(exports) {
+
+  // INSERT
+
+});
